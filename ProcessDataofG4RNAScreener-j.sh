cd658b70730142f554b588975192f1817eeb4a83
cyningj <94027057+cyningj@users.noreply.github.com>
2021-11-10 20:43:26 +0800

    Delete ProcessDataofG4RNAScreener-j.sh

--- ProcessDataofG4RNAScreener-j.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#去掉标题并将chrx:xxx-XXX转化为chr      xxx-XXX转化为chr        xxx     xxx
-ls > list
-sed -i 's/list//g'  list
-sed -i 's/ProcessDataofG4RNAScreener-j.sh//g'  list
-for i in `cat list`
-do
-        awk -F"\t" 'BEGIN{OFS="\t"}{gsub(/-/,"\t",$2);gsub(/:/,"\t",$2);print $0}' $i > $i"_clean"
-done
